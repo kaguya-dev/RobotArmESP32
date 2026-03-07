9962c1168b4711a264138834fcc631651c3d2fdb
KaguyaDev <enzodiaslima@gmail.com>
2026-03-07 00:43:21 -0300

    Code with first test and pin definitions (CLAW UNSETTED)

--- RobotArm/RobotArmDirect/RobotArmDirect.ino
+++ RobotArm/RobotArmDirect/RobotArmDirect.ino
@@ -3,22 +3,26 @@
 #define pin0 13
 #define pin1 12
 #define pin2 14
-#define pin3 0
+#define pin3 27
+#define pin4 26
 Servo baseServo;
 Servo s1,s2,s3;
+Servo sClaw;
 
 int xc, yc, zc, wc;
+bool claw;
 
 void setup() {
   Serial.begin(115200);
   delay(300);
 
-  Serial.println("Send: x y z w");
+  Serial.println("Send: x y z w and claw(0 = Closed | -1 = Opened)");
 
   xc = 0;
   yc = 0;
   zc = 0;
   wc = 0;
+  claw = 0;
 
   baseServo.setPeriodHertz(50);
   baseServo.attach(pin0);
@@ -32,6 +36,9 @@ void setup() {
   s3.setPeriodHertz(50);
   s3.attach(pin3);
   s3.write(90);
+  sClaw.setPeriodHertz(50);
+  sClaw.attach(pin4);
+  sClaw.write(90);
 }
 
 void loop() {
@@ -39,15 +46,16 @@ void loop() {
     String line = Serial.readStringUntil('\n');
     line.trim();
 
-    int x, y, z, w;
-    int parsed = sscanf(line.c_str(), "%d%*[ ,]%d%*[ ,]%d%*[ ,]%d", &x, &y, &z, &w);
+    int x, y, z, w, clawOp;
+    int parsed = sscanf(line.c_str(), "%d%*[ ,]%d%*[ ,]%d%*[ ,]%d%*[ ,]%d", &x, &y, &z, &w, &claw);
 
-    if (parsed == 4) {
+    if (parsed == 5) {
       xc = x;
       yc = y;
       zc = z;
       wc = w;
-
+      claw = claw;
+      
       xc = constrain(xc, 0, 180);
       yc = constrain(yc,  0, 180);
       zc = constrain(zc,  0, 180);
@@ -58,12 +66,16 @@ void loop() {
       s2.write(zc);
       s3.write(wc);
 
+      //if(claw) sClaw.write(180);
+      //else sClaw.write(0);
+
       Serial.println("Angle x:" + String(xc) +
                " |    Angle y:" + String(yc) +
                " |    Angle z:" + String(zc) +
-               " |    Angle w:" + String(wc));
+               " |    Angle w:" + String(wc) +
+               " |    Claw Op:" + String(claw));
     } else {
-      Serial.println("Invalid input. Use: x y z w");
+      Serial.println("Invalid input. Use: x y z w claw(0/1)");
     }
   }
 }
